ddeb35582abf86e7e7bb26a76f6e49f6cb3ddc06
songawee <dennis@songawee.com>
2016-06-25 18:52:42 -0600

    add firebase

---
+++ index.js
@@ -0,0 +1,17 @@
+const firebase = require("firebase");
+
+firebase.initializeApp({
+  serviceAccount: {
+    projectId: process.env.WHEN_TO_LEAVE_PROJECT_ID,
+    clientEmail: process.env.WHEN_TO_LEAVE_PROJECT_CLIENT_EMAIL,
+    privateKey: process.env.WHEN_TO_LEAVE_PRIVATE_KEY
+  },
+  databaseURL: process.env.WHEN_TO_LEAVE_DATABASE_URL
+});
+
+const db = firebase.database();
+const ref = db.ref("server/data");
+
+ref.set({
+  foo: 'blue bird'
+});
